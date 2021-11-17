f951827f818fab7caf3cfcd26f1d149c3ee7a503
Hazel Baker-Harvey <84642569+hazelbakerharvey@users.noreply.github.com>
2021-11-17 04:27:49 -0800

    added micrometer timer and counter, health check

--- loan-api/src/main/java/com/ss/eastcoderbank/loanapi/controller/ExceptionController.java
+++ loan-api/src/main/java/com/ss/eastcoderbank/loanapi/controller/ExceptionController.java
@@ -5,6 +5,10 @@ import com.ss.eastcoderbank.core.exeception.AccountNotEmptyException;
 import com.ss.eastcoderbank.core.exeception.AccountNotFoundException;
 import com.ss.eastcoderbank.core.exeception.UserNotFoundException;
 import com.ss.eastcoderbank.core.exeception.response.ErrorMessage;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,15 +17,29 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
 @ControllerAdvice
+@NoArgsConstructor
 public class ExceptionController {
+
+    private MeterRegistry meterRegistry;
+    private Counter serviceCounter;
+    private Timer serviceTimer;
+
+    ExceptionController(MeterRegistry meterRegistry){
+        this.meterRegistry = meterRegistry;
+        serviceCounter = meterRegistry.counter("PAGE_VIEWS.LoansErrors");
+        serviceTimer = meterRegistry.timer("execution.time.LoansErrors");
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> userValidationError(MethodArgumentNotValidException exception) {
+        long startTime = System.currentTimeMillis();
+        serviceCounter.increment();
         Map<String, String> errors = new HashMap<>();
         errors.put("status", HttpStatus.BAD_REQUEST.toString());
         errors.put("message", "");
@@ -32,36 +50,52 @@ public class ExceptionController {
             errors.put("message", errors.get("message") + " " + errorMessage);
         });
         log.warn(errors.toString());
+        serviceTimer.record(Duration.ofMillis((System.currentTimeMillis()-startTime)));
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<ErrorMessage> forbidden(AccessDeniedException exception) {
+        long startTime = System.currentTimeMillis();
+        serviceCounter.increment();
         log.warn("Access is forbidden, user does not have permission to access this resource.");
+        serviceTimer.record(Duration.ofMillis((System.currentTimeMillis()-startTime)));
         return new ResponseEntity<>(new ErrorMessage(HttpStatus.FORBIDDEN.toString(), exception.getMessage()), HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(JsonParseException.class)
     public ResponseEntity<ErrorMessage> jsonParseFailure(JsonParseException exception) {
+        long startTime = System.currentTimeMillis();
+        serviceCounter.increment();
         log.warn("JSON is not valid.");
+        serviceTimer.record(Duration.ofMillis((System.currentTimeMillis()-startTime)));
         return new ResponseEntity<>(new ErrorMessage(HttpStatus.BAD_REQUEST.toString(), "Not valid json. " +  exception.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorMessage> noUserFound(UserNotFoundException exception) {
+        long startTime = System.currentTimeMillis();
+        serviceCounter.increment();
         log.warn("User is not found.");
+        serviceTimer.record(Duration.ofMillis((System.currentTimeMillis()-startTime)));
         return new ResponseEntity<>(new ErrorMessage(HttpStatus.NOT_FOUND.toString(), exception.getMessage()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(AccountNotFoundException.class)
     public ResponseEntity<ErrorMessage> duplicateConstraints(AccountNotFoundException exception) {
+        long startTime = System.currentTimeMillis();
+        serviceCounter.increment();
         log.warn("Account is not found.");
+        serviceTimer.record(Duration.ofMillis((System.currentTimeMillis()-startTime)));
         return new ResponseEntity<>(new ErrorMessage(HttpStatus.NOT_FOUND.toString(), exception.getMessage()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(AccountNotEmptyException.class)
     public ResponseEntity<ErrorMessage> duplicateConstraints(AccountNotEmptyException exception) {
+        long startTime = System.currentTimeMillis();
+        serviceCounter.increment();
         log.warn("Account is not empty.");
+        serviceTimer.record(Duration.ofMillis((System.currentTimeMillis()-startTime)));
         return new ResponseEntity<>(new ErrorMessage(HttpStatus.PRECONDITION_FAILED.toString(), exception.getMessage()), HttpStatus.PRECONDITION_FAILED);
     }
 }
--- loan-api/src/main/java/com/ss/eastcoderbank/loanapi/controller/LoanController.java
+++ loan-api/src/main/java/com/ss/eastcoderbank/loanapi/controller/LoanController.java
@@ -6,7 +6,11 @@ import com.ss.eastcoderbank.core.repository.UserRepository;
 import com.ss.eastcoderbank.core.transferdto.LoanDto;
 import com.ss.eastcoderbank.loanapi.dto.CreateLoanDto;
 import com.ss.eastcoderbank.loanapi.service.LoanService;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -16,56 +20,90 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.Duration;
 import java.util.List;
 
 @Slf4j
 @CrossOrigin
 @RestController
 @RequestMapping("loans")
-@AllArgsConstructor
+@NoArgsConstructor
 public class LoanController {
 
     @Autowired
     LoanService loanService;
+    @Autowired
+    UserRepository userRepository;
+    private MeterRegistry meterRegistry;
+    private Counter serviceCounter;
+    private Timer serviceTimer;
 
-    private final UserRepository userRepository;
+    LoanController(MeterRegistry meterRegistry){
+        this.meterRegistry = meterRegistry;
+        serviceCounter = meterRegistry.counter("PAGE_VIEWS.Loans");
+        serviceTimer = meterRegistry.timer("execution.time.Loans");
+    }
 
     @GetMapping
     public ResponseEntity<Page<LoanDto>> getPaginatedLoans(@RequestParam(name="page") Integer pageNumber, @RequestParam(name="size") Integer pageSize, @RequestParam(value="asc", required = false) boolean asc, @RequestParam(value = "sort", required = false) String sort, Pageable page) {
+        long startTime = System.currentTimeMillis();
+        serviceCounter.increment();
         log.trace("get loan endpoint reached...");
         log.debug("Endpoint pageNumber: {}, pageSize: {}", pageNumber, pageSize);
+        serviceTimer.record(Duration.ofMillis((System.currentTimeMillis()-startTime)));
         return new ResponseEntity<>(loanService.getLoans(pageNumber, pageSize, asc, sort), HttpStatus.OK);
     }
 
     @GetMapping("/users/{id}/page")
     public ResponseEntity<Page<LoanDto>> getPaginatedLoansByUser(@PathVariable Integer id, @RequestParam(name="page") Integer pageNumber, @RequestParam(name="size") Integer pageSize, @RequestParam(value="asc", required = false) boolean asc, @RequestParam(value = "sort", required = false) String sort, Pageable page) {
+        long startTime = System.currentTimeMillis();
+        serviceCounter.increment();
         log.trace("get loans by user endpoint reached...");
         log.info("user id: {}", id);
         log.debug("Endpoint pageNumber: {}, pageSize: {}", pageNumber, pageSize);
         User user = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
+        serviceTimer.record(Duration.ofMillis((System.currentTimeMillis()-startTime)));
         return new ResponseEntity<>(loanService.getLoansByUserPaginated(user, pageNumber, pageSize, asc, sort), HttpStatus.OK);
     }
 
     @GetMapping("/users/{id}")
     public ResponseEntity<List<LoanDto>> getLoansByUser(@PathVariable Integer id) {
+        long startTime = System.currentTimeMillis();
+        serviceCounter.increment();
         log.trace("get loans by user endpoint reached...");
         log.info("user id: {}", id);
         User user = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
+        serviceTimer.record(Duration.ofMillis((System.currentTimeMillis()-startTime)));
         return new ResponseEntity<>(loanService.getLoansByUser(user), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<LoanDto> getLoansById(@PathVariable Integer id) {
+        long startTime = System.currentTimeMillis();
+        serviceCounter.increment();
         log.trace("get loans by id endpoint reached...");
         log.info("loan id: {}", id);
+        serviceTimer.record(Duration.ofMillis((System.currentTimeMillis()-startTime)));
         return new ResponseEntity<>(loanService.getLoansById(id), HttpStatus.OK);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createLoan(@Valid @RequestBody CreateLoanDto loan) {
+        long startTime = System.currentTimeMillis();
+        serviceCounter.increment();
         log.trace("POST loan endpoint reached...");
         loanService.createLoan(loan);
+        serviceTimer.record(Duration.ofMillis((System.currentTimeMillis()-startTime)));
+    }
+
+    @GetMapping("/health")
+    @ResponseStatus(HttpStatus.OK)
+    public void healthCheck() {
+        long startTime = System.currentTimeMillis();
+        serviceCounter.increment();
+        log.trace("HEALTH loan endpoint reached...");
+        serviceTimer.record(Duration.ofMillis((System.currentTimeMillis()-startTime)));
     }
 
 }
